007552c5656ebe162e247c92c01eab544841148f
Casey White <cwhite2019@gmail.com>
2020-06-13 12:43:32 -0400

    moves file down a directory

---
+++ target/debug/chip-8.d
@@ -0,0 +1,1 @@
+C:\Users\cwhit\Documents\chip-8\target\debug\chip-8.exe: C:\Users\cwhit\Documents\chip-8\src\main.rs
---
+++ target/debug/deps/chip_8-ad589914e3795b76.d
@@ -0,0 +1,5 @@
+C:\Users\cwhit\Documents\chip-8\chip-8\target\debug\deps\chip_8-ad589914e3795b76.exe: src\main.rs
+
+C:\Users\cwhit\Documents\chip-8\chip-8\target\debug\deps\chip_8-ad589914e3795b76.d: src\main.rs
+
+src\main.rs:
