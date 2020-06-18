7fe302cb3968d7ecdac6f6391b4bd4724595a4e1
Casey White <cwhite2019@gmail.com>
2020-06-17 21:35:29 -0400

    adds display and controller

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 extern crate rand;
+extern crate sdl2;
 
 use std::env;
 
 mod cpu;
 mod rom;
 mod font;
+mod video;
+mod input;
 
+use std::thread;
+use std::time::Duration;
 use cpu::CPU;
 use rom::Rom;
+use video::VideoWindow;
+use input::Input;
 
 
 const SCREEN_HEIGHT: usize = 32;
 const SCREEN_WIDTH: usize = 64;
-const SCREEN_SCALE: usize = 32;
-
 
 fn main() {
+
+    let sdl_context = sdl2::init().unwrap();
+    let sleep_duration = Duration::from_millis(1);
+
+    let mut display = VideoWindow::new(&sdl_context);
+    let mut input = Input::new(&sdl_context);
 
     let mut chip = CPU::new();
     let filename = env::args().nth(1);
@@ -25,16 +36,22 @@ fn main() {
             Rom::new(&filename)
         },
         None => {
-            panic!("Uh Oh");
+            panic!("Uh Oh: File not read");
         }
     };
 
     chip.load_rom(current_rom);
 
-    for (i, &byte) in chip.memory.iter().enumerate() {
-        if byte > 0 {
-            println!("{}: {}", i, byte)
+    while let Ok(keypad) = input.poll() {
+
+        let state = chip.tick(keypad);
+
+        if state.video_changed {
+            display.draw(state.video_buffer);
         }
+
+        thread::sleep(sleep_duration);
+
     }
     
     
